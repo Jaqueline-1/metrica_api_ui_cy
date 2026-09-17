b360d5003aec2fa5ff6196e42aef40d6481ba13d
Jaqueline Andrade <kelinegyn@gmail.com>
2026-09-17 15:49:47 -0300

    atualização dos estudos

--- teste_api_ebac/hub-de-leitura-api-teste-task/cypress.config.js
+++ teste_api_ebac/hub-de-leitura-api-teste-task/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'yc4s14',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
